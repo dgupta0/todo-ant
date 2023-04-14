ba7ac995a59082aee8faf32e14ca760f3022ca5e
Deeti <deetigupta8@gmail.com>
2023-04-14 19:07:27 +0530

    server setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 
 
 function App() {
+  const [data, setData] = useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/todos")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setData(data)
+      })
+  }, [])
+
+  console.log(data)
 
   return (
     <h1>Ant Todo</h1>
