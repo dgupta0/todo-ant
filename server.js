08a269f9a93485fd1e381b2ae6a18d6f6f5c5c6d
Deeti <deetigupta8@gmail.com>
2023-04-14 15:51:50 +0530

    basic mirage server setup

---
+++ server.js
@@ -0,0 +1,29 @@
+import { createServer } from "miragejs"
+
+createServer({
+    routes() {
+        this.namespace = "api"
+
+        this.get("/todos", () => {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        timeStamp: "2023-04-05",
+                        title: "Read",
+                        description: "read a book",
+                        status: "Open"
+                    },
+                    {
+                        id: 2,
+                        timeStamp: "2023-04-08",
+                        title: "Write",
+                        description: "write a blog",
+                        tag: ["marketing", "finance"],
+                        status: "Open"
+                    }
+                ],
+            }
+        })
+    },
+})
--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import viteLogo from '/vite.svg'
 function App() {
 
   return (
-    <h1>Basic Test</h1>
+    <h1>Ant Todo</h1>
   )
 }
 
