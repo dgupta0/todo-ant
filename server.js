82c3ffe17cca9b8c7495b26b1a1b69998ec05297
David Rosendorf <rosendorf.dev@gmail.com>
2023-04-20 06:57:56 +0000

    filters applied

--- server.js
+++ server.js
@@ -12,17 +12,99 @@ createServer({
                         timeStamp: "2023-04-05",
                         title: "Read",
                         description: "read a book",
-                        status: "Open"
+                        tags: [],
+                        status: "Working"
                     },
                     {
                         id: 2,
                         timeStamp: "2023-04-08",
                         title: "Write",
                         description: "write a blog",
-                        tag: ["marketing", "finance"],
+                        tags: ["marketing", "finance"],
                         status: "Open"
-                    }
+                    },
+                    {
+                        id: 3,
+                        timeStamp: "2023-04-10",
+                        title: "Exercise",
+                        description: "go for a run",
+                        tags: [],
+                        status: "Open"
+                      },
+                      {
+                        id: 4,
+                        timeStamp: "2023-04-12",
+                        title: "Grocery Shopping",
+                        description: "buy groceries for the week",
+                        tags: [],
+                        status: "Open"
+                      },
+                      {
+                        id: 5,
+                        timeStamp: "2023-04-14",
+                        title: "Appointment",
+                        description: "go to the dentist",
+                        tags: [],
+                        status: "Open"
+                      },
+                      {
+                        id: 6,
+                        timeStamp: "2023-04-15",
+                        title: "Call",
+                        description: "call mom",
+                        tags: [],
+                        status: "Overdue"
+                      },
+                      {
+                        id: 7,
+                        timeStamp: "2023-04-17",
+                        title: "Research",
+                        description: "research vacation destinations",
+                        tags: [],
+                        status: "Open"
+                      },
+                      {
+                        id: 8,
+                        timeStamp: "2023-04-20",
+                        title: "Meeting",
+                        description: "meet with the team",
+                        tags: ["work"],
+                        status: "Open"
+                      },
+                      {
+                        id: 9,
+                        timeStamp: "2023-04-22",
+                        title: "Cooking",
+                        description: "make dinner",
+                        tags: ["food"],
+                        status: "Done"
+                      },
+                      {
+                        id: 10,
+                        timeStamp: "2023-04-25",
+                        title: "Gardening",
+                        description: "plant some flowers",
+                        tags: ["home"],
+                        status: "Open"
+                      },
+                      {
+                        id: 11,
+                        timeStamp: "2023-04-28",
+                        title: "Shopping",
+                        description: "buy a new dress",
+                        tags: ["fashion"],
+                        status: "Overdue"
+                      },
+                      {
+                        id: 12,
+                        timeStamp: "2023-04-30",
+                        title: "Read",
+                        description: "start a new book",
+                        tags: ["books"],
+                        status: "Overdue"
+                      }
                 ],
+                
             }
         })
     },
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,109 @@
-import { useState } from 'react'
-import React from 'react'
-
+import { useState, useEffect } from "react";
+import { Table, Tag } from "antd";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [todos, setTodos] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetch("/api/todos")
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setData(data)
-      })
-  }, [])
+      .then((res) => res.json())
+      .then(data => { setTodos(data.todos); });
+  }, []);
 
-  console.log(data)
+  // We know there are going to be only these 4 statuses
+  // There is no need to get them dynamicaly from todos
+  const statusFilters = ["Open", "Working", "Overdue", "Done"];
+  /* 
+  --------------------------------------------------
+  The following code can be separated into 4 parts
+  --------------------------------------------------
+  1) Create new Set, uniqueTags, to store unique tags - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+  2) Traverse todos array and for each todo add all its tags to uniqueTags
+  3) Covert the uniqueTags Set to Array
+  4) Map each tag to the final {text, value} object
+  --------------------------------------------------
+  It could be also implemented like the code below.
+  --------------------------------------------------
+  let tagFilters = [];
+  if (todos) {
+    // 1)
+    const uniqueTags = new Set();
+    // 2)
+    todos.forEach((todo) => {
+      for (const tag of todo.tags) {
+        uniqueTags.add(tag);
+      }
+    });
+    // 3)
+    const uniqueTagsArray = Array.from(uniqueTags) // [...uniqueTags] would also work
+    // 4)
+    tagFilters = uniqueTagsArray.map((tag) => { 
+      return {
+        text: tag[0].toUpperCase() + tag.slice(1),
+        value: tag,
+      }
+    })
+  }
+  --------------------------------------------------
+  */
+  
 
-  return (
-    <>
-      {data && <p>{JSON.stringify(data)}</p>}
-    </>
-  )
+  const tagsFilters = todos
+    ? Array.from(
+        todos.reduce((uniqueTags, { tags }) => {
+          for (const tag of tags) {
+            uniqueTags.add(tag);
+          }
+          return uniqueTags;
+        }, new Set())
+      ).map((tag) => ({
+        text: tag[0].toUpperCase() + tag.slice(1),
+        value: tag,
+      }))
+    : [];
+
+  const columns = [
+    {
+      title: "Timestamp created",
+      dataIndex: "timeStamp",
+      key: "timeStamp",
+    },
+    {
+      title: "Task",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Tags",
+      dataIndex: "tags",
+      key: "tags",
+      filters: tagsFilters,
+      onFilter: (value, { tags }) => tags.includes(value),
+      render: (tags, { id }) => {
+        return (
+          <>
+            {tags.map((tag) => (
+              <Tag key={tag + id}>{tag.toUpperCase()}</Tag>
+            ))}
+          </>
+        );
+      },
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      filters: statusFilters,
+      onFilter: (value, { status }) => status === value,
+    },
+  ];
+
+  return <Table columns={columns} dataSource={todos} />;
 }
 
-export default App
+export default App;
