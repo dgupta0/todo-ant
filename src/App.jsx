ec43fc7598cc7b353108ee037e5e511497455a58
Deeti <deetigupta8@gmail.com>
2023-04-22 14:23:34 +0530

    implementing search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import React from 'react'
-import { Table, Tag } from 'antd';
+import { Table, Tag, Input } from 'antd';
 
+
+const { Search } = Input;
 
 function App() {
   const [data, setData] = useState(null);
+  const [filterVal, setFilterVal] = useState("")
+
 
   React.useEffect(() => {
     fetch("/api/todos")
@@ -116,8 +120,38 @@ function App() {
       }
     ]
 
+  function handleFilterVal(e) {
+    setFilterVal(e.target.value)
+  }
+
+  function handleClick() {
+    console.log(filterVal)
+    setData(prev => {
+      let newTodo = []
+      prev.forEach(todo => {
+        if (todo.title.toLowerCase().includes(filterVal.toLowerCase())) {
+          newTodo.push(todo)
+        }
+      })
+      return newTodo
+    })
+  }
+
   return (
-    <Table dataSource={data} columns={columns} rowKey={"id"} />
+    <>
+      <Search
+        className='search'
+        placeholder="input search text"
+        allowClear
+        enterButton="Search"
+        size="large"
+        onChange={handleFilterVal}
+        onSearch={handleClick}
+      />
+      {/* <Search class="search" onChange={handleFilterVal} onPressEnter={handleClick} /> */}
+      <Table dataSource={data} columns={columns} rowKey={"id"} />
+    </>
+
   )
 }
 
