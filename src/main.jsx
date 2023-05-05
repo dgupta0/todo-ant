caf4686f83e14376589960a2f7c82a83d3dd39bd
Deeti <deetigupta8@gmail.com>
2023-05-05 13:30:39 +0530

    bug resolved

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import "../server"
+import server from "../server"
+
+server()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
